98cce0151ecbc666cfce179fbf35a1d748455b25
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-01-17 09:21:14 +0100

    initial commit

---
+++ src/components/providers.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "./theme-provider";
+
+export default function Providers({children}: {children: ReactNode}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
