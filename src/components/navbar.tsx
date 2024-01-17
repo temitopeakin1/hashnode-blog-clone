4afc4c647cab2bfdbc65289fdd6ddaf50ad3a43f
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-01-17 09:28:41 +0100

    navbar update

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+const GITHUB_URL = "https://github.com/temitopeakin1/hashnode-blog-clone";
+
+export default function Navbar() {
+  return <div className="w-full border-b mb-5">
+    <div className="max-w-7xl w-full px-3 xl:p-0 my-5 mx-auto flex justify-between items-center">
+        
+    </div>
+  </div>;
+}
