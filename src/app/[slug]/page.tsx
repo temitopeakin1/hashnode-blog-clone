281f9fd0670e3e56ed5eaf12c44902e10e67872a
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-02-26 19:30:48 +0100

    update

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,13 @@
-// Import necessary dependencies
 import Post from "@/components/post";
 import { getPostBySlug } from "@/lib/request";
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 
-// Function to generate metadata
-export async function generateMetaData({
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
+export async function generateMetadata({
   params,
 }: {
   params: { slug: string };
@@ -15,27 +18,23 @@ export async function generateMetaData({
   };
 }
 
-// Page component
-export default async function BlogPage({ params }: { params?: { slug: string } }) {
-  // Create a new QueryClient instance
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const queryClient = new QueryClient();
 
-  // Add a conditional check for params before prefetching the query
-  if (params?.slug) {
-    // Prefetch the query for better performance
-    await queryClient.prefetchQuery({
-      queryKey: ["post", params.slug],
-      queryFn: () => getPostBySlug(params.slug),
-    });
-  }
+  await queryClient.prefetchQuery({
+    queryKey: ["post", params.slug],
+    queryFn: () => getPostBySlug(params.slug),
+  });
 
-  // Return the JSX with the HydrationBoundary wrapping the component tree
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="max-w-7xl w-full px-3 xl:p-0 mx-auto">
-        {/* Use nullish coalescing to provide a default value */}
-        <Post slug={params?.slug ?? 'default-slug'} />
-      </div>
-    </HydrationBoundary>
+    <div className="max-w-7xl w-full px-3 xl:p-0 mx-auto">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Post slug={params.slug} />
+      </HydrationBoundary>
+    </div>
   );
 }
