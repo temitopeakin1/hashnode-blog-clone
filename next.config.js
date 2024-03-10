9cc9a5e2b9302eac274d23e237ef33f197382e02
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-03-10 21:40:22 +0100

    update

--- next.config.js
+++ next.config.js
@@ -9,19 +9,3 @@ module.exports = nextConfig
 
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//       domains: ['cdn.hashnode.com'],
-//     },
-//   };
-  
-//   module.exports = nextConfig;
-
-  // next.config.js
-
-// module.exports = {
-//   images: {
-//     domains: ['cdn.hashnode.com'],
-//   },
-// };
